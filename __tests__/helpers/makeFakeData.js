28abb314463ca5dd1f75d588abd4972d021660c7
Danil Volkov <volchara287@gmail.com>
2024-05-30 01:49:44 +0400

    add: создатель фейковых данных для tasks

--- __tests__/helpers/makeFakeData.js
+++ __tests__/helpers/makeFakeData.js
@@ -7,4 +7,12 @@ const fakeUser = {
   email: faker.internet.email(),
 };
 
-console.log(fakeUser);
+const fakeTask = {
+  name: faker.person.jobTitle(),
+  description: faker.lorem.sentence(4),
+  statusId: faker.number.int({ min: 1, max: 2 }),
+  creatorId: faker.number.int({ min: 1, max: 3 }),
+  executorId: faker.number.int({ min: 0, max: 3 }),
+};
+
+console.log(fakeTask);
