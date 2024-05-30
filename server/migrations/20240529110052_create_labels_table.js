08a5b6efda74bd544c4f9c6268367e0f13539859
Danil Volkov <volchara287@gmail.com>
2024-05-30 04:24:10 +0400

    fix: возвращаю обратно даты создания меток

--- server/migrations/20240529110052_create_labels_table.js
+++ server/migrations/20240529110052_create_labels_table.js
@@ -4,6 +4,8 @@ export const up = (knex) => (
   knex.schema.createTable('labels', (table) => {
     table.increments('id').primary();
     table.string('name').unique();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
 );
 
