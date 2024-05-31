966777cf98d426ba56df471a933f93f2519122db
Danil Volkov <volchara287@gmail.com>
2024-06-01 03:32:04 +0400

    add: миграция с altertable tasks

--- server/migrations/20240531230725_references_table.js
+++ server/migrations/20240531230725_references_table.js
@@ -7,28 +7,23 @@ export const up = (knex) =>
       table.integer('label_id');
       table.integer('task_id');
     })
-    .createTable('tasks', (table) => {
-      table.increments('id').primary();
-      table.string('name');
-      table.string('description');
-  
-      table.integer('status_id')
+    .alterTable('tasks', (table) => {
+      table
+        .integer('status_id')
         .references('id')
         .inTable('statuses')
         .onDelete('RESTRICT');
-  
-      table.integer('creator_id')
+      table
+        .integer('creator_id')
         .references('id')
         .inTable('users')
         .onDelete('RESTRICT');
-  
-      table.integer('executor_id')
+      table
+        .integer('executor_id')
         .references('id')
         .inTable('users')
         .onDelete('SET NULL');
   
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
     })
 
 export const down = (knex) => knex.schema.dropTable('labels_for_tasks');
