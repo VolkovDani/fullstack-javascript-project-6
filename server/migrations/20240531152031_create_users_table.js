90b0a48487a52a51fedf18d0d4de0243b80bb81a
Danil Volkov <volchara287@gmail.com>
2024-05-31 19:59:53 +0400

    fix: вернул в исходное состояние таблицу users
    Теперь нет проверки на наличие таблицы со статусами

--- server/migrations/20240531152031_create_users_table.js
+++ server/migrations/20240531152031_create_users_table.js
@@ -1,17 +1,13 @@
-// @ts-check
 
 export const up = (knex) => (
-  knex.schema.hasTable('statuses').then((exists) => {
-    if (exists) return knex.schema.createTable('users', (table) => {
-      table.increments('id').primary();
-      table.string('email');
-      table.string('password_digest');
-      table.string('first_name');
-      table.string('last_name');
-      table.timestamp('created_at').defaultTo(knex.fn.now());
-      table.timestamp('updated_at').defaultTo(knex.fn.now());
-    });
-  
+  knex.schema.createTable('users', (table) => {
+    table.increments('id').primary();
+    table.string('email');
+    table.string('password_digest');
+    table.string('first_name');
+    table.string('last_name');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
 );
 
