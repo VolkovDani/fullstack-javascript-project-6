eb38de43449f191d11ffa0d5e13d80818e07c9e2
Danil Volkov <volchara287@gmail.com>
2024-05-31 20:26:37 +0400

    fix: создание второй таблицы происходит не в цепочке промиса, а в теле then

--- server/migrations/20240531155600_create_statuses_and_tasks_table.js
+++ server/migrations/20240531155600_create_statuses_and_tasks_table.js
@@ -6,26 +6,27 @@ export const up = (knex) => (
     table.string('name', 50).unique();
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
-  }).createTable('tasks', (table) => {
-    table.increments('id').primary();
-    table.string('name');
-    table.string('description');
-
-    table.integer('status_id')
-      .references('id')
-      .inTable('statuses')
-      .onDelete('RESTRICT');
-
-    table.integer('creator_id')
-      .references('id')
-      .inTable('users')
-      .onDelete('RESTRICT');
-
-    table.integer('executor_id')
-
-    table.timestamp('created_at').defaultTo(knex.fn.now());
-    table.timestamp('updated_at').defaultTo(knex.fn.now());
-  })
+  }).then(() => knex.schema.createTable('tasks', (table) => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('description');
+  
+      table.integer('status_id')
+        .references('id')
+        .inTable('statuses')
+        .onDelete('RESTRICT');
+  
+      table.integer('creator_id')
+        .references('id')
+        .inTable('users')
+        .onDelete('RESTRICT');
+  
+      table.integer('executor_id')
+  
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+  )
 );
 
 export const down = (knex) => knex.schema.dropTable('statuses');
