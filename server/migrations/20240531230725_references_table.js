069bdc597ca526a9a593cb85f7e2784dba8911a3
Danil Volkov <volchara287@gmail.com>
2024-06-01 11:16:17 +0400

    fix: правка ошибок eslint

--- server/migrations/20240531230725_references_table.js
+++ server/migrations/20240531230725_references_table.js
@@ -1,23 +1,22 @@
 // @ts-check
 
-export const up = (knex) =>
-  knex.schema
-    .createTable('labels_for_tasks', (table) => {
-      table.increments('id').primary();
-      table.integer('label_id');
-      table.integer('task_id');
-    })
-    .alterTable('tasks', (table) => {
-      table
-        .integer('status_id')
-        .references('id')
-        .inTable('statuses')
-        .onDelete('RESTRICT');
-      table
-        .integer('creator_id')
-        .references('id')
-        .inTable('users')
-        .onDelete('RESTRICT');
-    });
+export const up = (knex) => knex.schema
+  .createTable('labels_for_tasks', (table) => {
+    table.increments('id').primary();
+    table.integer('label_id');
+    table.integer('task_id');
+  })
+  .alterTable('tasks', (table) => {
+    table
+      .integer('status_id')
+      .references('id')
+      .inTable('statuses')
+      .onDelete('RESTRICT');
+    table
+      .integer('creator_id')
+      .references('id')
+      .inTable('users')
+      .onDelete('RESTRICT');
+  });
 
 export const down = (knex) => knex.schema.dropTable('labels_for_tasks');
