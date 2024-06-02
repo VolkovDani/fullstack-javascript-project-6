aba24c2425bbfbb599b0fc4b50425fb6d66a42f6
Danil Volkov <volchara287@gmail.com>
2024-06-02 19:17:37 +0400

    add: Откат для альтера таблицы Fixes #3

--- server/migrations/20240531230725_references_table.js
+++ server/migrations/20240531230725_references_table.js
@@ -5,18 +5,6 @@ export const up = (knex) => knex.schema
     table.increments('id').primary();
     table.integer('label_id');
     table.integer('task_id');
-  })
-  .alterTable('tasks', (table) => {
-    table
-      .integer('status_id')
-      .references('id')
-      .inTable('statuses')
-      .onDelete('RESTRICT');
-    table
-      .integer('creator_id')
-      .references('id')
-      .inTable('users')
-      .onDelete('RESTRICT');
   });
 
 export const down = (knex) => knex.schema.dropTable('labels_for_tasks');
